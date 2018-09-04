c969f9a7e9ecbde69ef1dd8888bbeb150cc5f78b
W10-01706171740\Administrator <123@123.com>
2018-09-04 22:37:01 +0800

    add package-lock.json

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -11,8 +11,8 @@ let a = style;
 class Greeting extends React.Component {
     render() {
       return <div className="wrap">
-      <div styleName="style.h1">Hello main red component11111 <svg><use xlinkHref="#G:\git projects\itinerary\src\components\main\set.svg" x="50" y="50" /></svg><input placeholder="synchronous"/></div>
-      <div styleName="style.h2">Hello main green component <img src={pic}/><svg><use xlinkHref="#G:\git projects\itinerary\src\images\form.svg" x="50" y="50" /></svg><input placeholder="synchronous"/></div>
+      <div styleName="style.h1">Hello main red component11111 <svg><use xlinkHref="#main-set" x="50" y="50" /></svg><input placeholder="synchronous"/></div>
+      <div styleName="style.h2">Hello main green component <img src={pic}/><svg><use xlinkHref="#images-form" x="50" y="50" /></svg><input placeholder="synchronous"/></div>
       </div>;
     }
   }
--- webpack_config/webpack.config.common.js
+++ webpack_config/webpack.config.common.js
@@ -52,7 +52,7 @@ module.exports = {
         test: /\.svg$/,
         loader: 'svg-sprite-loader',
         options:{
-          symbolId: (p) => p
+          symbolId: '[folder]-[name]'
         }
       },
       {
