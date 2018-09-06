89a77316eb3d25159d289b62bb5ee0f534a853df
W10-01706171740\Administrator <123@123.com>
2018-09-06 23:40:00 +0800

    add chunkfilename in webpack

--- webpack_config/webpack.config.common.js
+++ webpack_config/webpack.config.common.js
@@ -42,6 +42,7 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename:'bundle.[chunkhash].js',
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/'
   }
