beef60d328f4f7998fd104fe7b36ec7c35dcd580
W10-01706171740\Administrator <123@123.com>
2018-10-14 23:55:46 +0800

    delete unused comments

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,11 +4,6 @@ import './index.noneModules.css';
 import { CarouselCustom } from '../../components/carousel';
 import {config} from '../../common/ajaxConfig.js';
 import axios from 'axios';
-// import aa from '../../images/15380434541828.jpeg';
-// import ab from '../../images/15381044615697.jpeg';
-// import ac from '../../images/15381045921996.jpeg';
-// import ad from '../../images/15381046688621.jpeg';
-// import ae from '../../images/15381048978867.jpeg';
 
 let a = style;
 class Main extends React.Component<any,any> {
@@ -23,7 +18,6 @@ class Main extends React.Component<any,any> {
   }
   fetchCarouselData(){
     axios.get(config.mainDomain+'/mainPageSlideData.json').then( (response) => {
-      // console.log(response.data);
       this.setState({carouselData:response.data})
     })
     .catch(function (error) {
