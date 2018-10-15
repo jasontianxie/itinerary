e89973462317bbb099c5390f412c2a6dc8cbcc1b
jason <1986tianxie@sina.com>
2018-10-15 16:20:40 +0800

    add tabs and datePicker from antd

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,14 +4,19 @@ import './index.noneModules.css';
 import { CarouselCustom } from '../../components/carousel';
 import {config} from '../../common/ajaxConfig.js';
 import axios from 'axios';
+import { Tabs, DatePicker } from 'antd';
 
 let a = style;
+const TabPane = Tabs.TabPane;
+const {  RangePicker } = DatePicker;
+
 class Main extends React.Component<any,any> {
   constructor(props:any){
     super(props);
     this.state = {
       carouselData:[1,2,3,4,5]
     }
+    this.datePickerOnChange = this.datePickerOnChange.bind(this);
   }
   componentWillMount(){
     this.fetchCarouselData();
@@ -24,10 +29,23 @@ class Main extends React.Component<any,any> {
       console.log(error);
     });
   }
+  datePickerOnChange(date:any,dateString:any){
+    console.log(date, dateString);
+  }
   render() {
     return (<div styleName="style.wrap">
-              <CarouselCustom slideData={this.state.carouselData}/>
-            </div>);
+      <div>
+        <CarouselCustom slideData={this.state.carouselData} />
+        <div styleName="style.tabWrap">
+          <Tabs type="card" size="small">
+            <TabPane tab="Tab 1" key="1">
+              <RangePicker onChange={this.datePickerOnChange} showTime={true} format="YYYY-MM-DD HH:mm:ss" placeholder={['startDateTime','endDateTime']}/>
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">Content of Tab Pane 2</TabPane>
+          </Tabs>
+        </div>
+      </div>
+    </div>);
   }
 }
 export { Main };
