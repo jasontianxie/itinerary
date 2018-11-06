93e38af2b113a635589c2f7217c4597f88a02bd2
jason <1986tianxie@sina.com>
2018-11-06 18:54:14 +0800

    add a new route

--- src/pages/second/index.tsx
+++ src/pages/second/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './index.scss';
-import { List, Form, Input, DatePicker, Button } from 'antd';
+import { List, Form, Input, InputNumber, DatePicker, Button, Col, Select } from 'antd';
 
 const data = [
   {startSpot:'chonging',endSpot:'chengdu',startTime:'2018-11-01 12:00:00',endTime:'2018-11-01 13:00:00',spentTime:'0d1h0m0s',waitTime:'1h',vehicle:'高铁',cost:'100RMB',comments:''},
@@ -11,20 +11,42 @@ const data = [
 ];
 const FormItem = Form.Item;
 const { RangePicker } = DatePicker;
+const Option = Select.Option;
+const { TextArea } = Input;
 
 class Second extends React.Component<any,any> {
     constructor(props:any){
       super(props);
+      this.state = {
+        waitTimeHours:0,
+        waitTimeMinutes:0,
+        cost:0
+      }
       this.datePickerOnChange = this.datePickerOnChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.setWaitTimeHours = this.setWaitTimeHours.bind(this);
+      this.setWaitTimeMinutes = this.setWaitTimeMinutes.bind(this);
+      this.setCost = this.setCost.bind(this);
     }
     componentWillMount(){
       document.title = '新增游记'
     }
-    handleSubmit(){
-
+    handleSubmit(e:any){
+      // e.persist();
+      e.preventDefault();
+      console.log(this.state);
     }
     datePickerOnChange(date: any, dateString: any) {
       console.log(date, dateString);
+    }
+    setWaitTimeHours(value:any){
+      this.setState({waitTimeHours:value})
+    }
+    setWaitTimeMinutes(value:any){
+      this.setState({waitTimeMinutes:value})
+    }
+    setCost(value:any){
+      this.setState({cost:value})
     }
     render() {
       const { getFieldDecorator } = this.props.form;
@@ -55,15 +77,15 @@ class Second extends React.Component<any,any> {
             offset: 0,
           },
           sm: {
-            span: 16,
-            offset: 8,
+            span: 20,
+            offset: 4,
           },
         },
       };
 
       return (
       <div className="wrap">
-        <div styleName="detailRecord">这里是写游记区域</div>
+        <div styleName="detailRecord">this panel is for itinerary details</div>
           <div styleName="itinerary">
             <Form onSubmit={this.handleSubmit}>
               <FormItem
@@ -112,33 +134,64 @@ class Second extends React.Component<any,any> {
                 label="等待时间"
                 {...formItemLayout}
               >
-                {getFieldDecorator('waitHours', {})(
-                  <Input styleName="waitTime"/>
-                )}<span>小时</span>
-                {getFieldDecorator('waitMinutes', {})(
-                  <Input styleName="waitTime"/>
-                )}<span>分钟</span>
+                <Col span={6}>
+                  <FormItem>
+                    <InputNumber styleName="waitTime" min={0} onChange={this.setWaitTimeHours} defaultValue={0}/>
+                  </FormItem>
+                </Col>
+                <Col span={3}>
+                  <span>小时</span>
+                </Col>
+                <Col span={6}>
+                  <FormItem>
+                    <InputNumber styleName="waitTime" min={0} onChange={this.setWaitTimeMinutes} defaultValue={0}/>
+                  </FormItem>
+                </Col>
+                <Col span={3}>
+                  <span>分钟</span>
+                </Col>
               </FormItem>
               <FormItem
                 label="交通工具"
                 {...formItemLayoutTwoInRow}
               >
-                222
+                <Select defaultValue="flight">
+                  <Option value="flight">飞机</Option>
+                  <Option value="train">火车</Option>
+                  <Option value="CRH">高铁动车</Option>
+                  <Option value="LongBus">长途汽车</Option>
+                  <Option value="bus">公交车</Option>
+                  <Option value="metro">地铁</Option>
+                  <Option value="lightRailway">轻轨</Option>
+                  <Option value="taxi">出租车</Option>
+                  <Option value="drive">自驾</Option>
+                  <Option value="bike">自行车</Option>
+                  <Option value="moterBike">摩托车</Option>
+                  <Option value="electricBike">电动车</Option>
+                  <Option value="walk">步行</Option>
+                  <Option value="ferry">渡船</Option>
+                  <Option value="barge">游艇</Option>
+                  <Option value="moterShip">快艇</Option>
+                  <Option value="hangedBus">缆车</Option>
+                  <Option value="carriage">马车</Option>
+                  <Option value="bull-carriage">牛车</Option>
+                  <Option value="others">其他</Option>
+                </Select>
               </FormItem>
               <FormItem
                 label="费用"
                 {...formItemLayoutTwoInRow}
               >
-                222
+                <InputNumber styleName="waitTime" min={0} defaultValue={0} onChange={this.setCost}/>RMB
               </FormItem>
               <FormItem
                 label="备注"
                 {...formItemLayout}
               >
-                222
+                <TextArea rows={4} placeholder="输入关于该行程的相关信息，比如：公交是几路车，地铁是几号线，航班号等"/>
               </FormItem>
               <FormItem {...tailFormItemLayout}>
-                    <Button type="primary" htmlType="submit">搜索</Button>
+                    <Button type="primary" htmlType="submit">提交</Button>
                   </FormItem>
             </Form>
             <List
