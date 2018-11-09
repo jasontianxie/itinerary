c46c5879d464322e3710e8142fa5c087c1375e7a
jason <1986tianxie@sina.com>
2018-11-09 15:53:54 +0800

    add spread operator（添加es6扩展运算符的支持）

--- redux/reducers/mainPageState.js
+++ redux/reducers/mainPageState.js
@@ -8,17 +8,14 @@ export const mainPageState = (state=initState,action)=>{
         case 'MAIN_PAGE_GET_SEARCH_START':
         console.log('get data startting');
         return state;
-        break;
         case 'MAIN_PAGE_GET_SEARCH_SUCCESS':
         if (action.index === 1){
             return {...state,dataSource1:action.payload}
         } else {
             return {...state,dataSource2:action.payload}
         }
-        break;
         case 'MAIN_PAGE_GET_SEARCH_START':
         return state;
-        break;
         default:
         return state;
     }
--- webpack_config/webpack.config.common.js
+++ webpack_config/webpack.config.common.js
@@ -24,6 +24,12 @@ module.exports = {
             }
           }
         ]
+      },{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.svg$/,
