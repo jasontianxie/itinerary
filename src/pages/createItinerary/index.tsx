49c25c83fe5efe5a79ac7875e4c00769d6a2f2c9
jason <1986tianxie@sina.com>
2019-01-29 16:04:09 +0800

    动态添加和删除目的地

--- src/pages/createItinerary/index.tsx
+++ src/pages/createItinerary/index.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import DocumentTitle from "react-document-title";
 import "./index.scss";
 import BMap from "BMap";
-import { Form, Input, InputNumber, DatePicker, Button, Col, Select, AutoComplete } from "antd";
+import { Form, Input, InputNumber, DatePicker, Button, Col, Select, AutoComplete, Icon } from "antd";
 import LazyOptions from "../../components/cascader";
 import axios from "axios";
 import { config } from "../../common/ajaxConfig.js";
 import Cookies from "js-cookie";
 
+let id = 1;
 const FormItem = Form.Item;
 const { RangePicker } = DatePicker;
 const AOption = AutoComplete.Option;
@@ -50,6 +51,30 @@ class CreateItinerary extends React.Component<any, any> {
     public spotNameChange(index: number, value: any) {
         console.log("spotNameChange");
     }
+    public remove = (k: any) => {
+        const { form } = this.props;
+        // can use data-binding to get
+        const keys = form.getFieldValue("keys");
+        // We need at least one passenger
+        if (keys.length === 1) {
+          return;
+        }
+        // can use data-binding to set
+        form.setFieldsValue({
+          keys: keys.filter((key: any) => key !== k),
+        });
+      }
+    public add = () => {
+        const { form } = this.props;
+        // can use data-binding to get
+        const keys = form.getFieldValue("keys");
+        const nextKeys = keys.concat(++id);
+        // can use data-binding to set
+        // important! notify form to detect changes
+        form.setFieldsValue({
+          keys: nextKeys,
+        });
+      }
     public componentDidMount() {
         const map = new BMap.Map(this.baiduMap.current);
         const point = new BMap.Point(116.404, 39.915);
@@ -73,24 +98,24 @@ class CreateItinerary extends React.Component<any, any> {
         });
     }
     public render() {
-        const { getFieldDecorator } = this.props.form;
+        const { getFieldDecorator, getFieldValue } = this.props.form;
         const children1 = <AOption key="111">111</AOption>;
         const children2 = <AOption key="222">222</AOption>;
         const formItemLayout = {
             labelCol: {
-                sm: { span: 6 },
+                sm: { span: 8 },
                 xs: { span: 24 },
             },
             wrapperCol: {
-                sm: { span: 18 },
+                sm: { span: 16 },
                 xs: { span: 24 },
             },
         };
         const tailFormItemLayout = {
             wrapperCol: {
                 sm: {
-                    offset: 6,
-                    span: 18,
+                    offset: 8,
+                    span: 16,
                 },
                 xs: {
                     offset: 0,
@@ -98,6 +123,49 @@ class CreateItinerary extends React.Component<any, any> {
                 },
             },
         };
+        const formItemLayoutWithOutLabel = {
+            wrapperCol: {
+              xs: { span: 24, offset: 0 },
+              sm: { span: 16, offset: 8 },
+            },
+          };
+        getFieldDecorator("keys", { initialValue: [1] });
+        const keys = getFieldValue("keys");
+        const formItems = keys.map((k: any, index: any) => (
+            <div styleName="new-item" key={index}>
+                <div styleName = "form-item-wrap">
+                    <FormItem
+                        label={`选择目的地${index + 1}`}
+                        {...formItemLayout}
+                    >
+                        <LazyOptions getlazyloadCascader={this.getlazyloadCascader} index={2} />
+                    </FormItem>
+                    <FormItem
+                        label={`目的地${index + 1}具体名称`}
+                        {...formItemLayout}
+                    >
+                        {getFieldDecorator("endSpot", {
+                            rules: [{
+                                message: "输入目的地!", required: true,
+                            }],
+                        })(
+                            <AutoComplete
+                                style={{ width: "100%" }}
+                                onSelect={(value) => this.onSelect(2, value)}
+                                onSearch={(value) => this.handleSearch(2, value)}
+                                onChange={(value) => this.spotNameChange(2, value)}
+                                placeholder={`目的地${index + 1}地点名称`}
+                            >
+                                {children2}
+                            </AutoComplete>,
+                        )}
+                    </FormItem>
+                </div>
+                {keys.length > 1 ? (
+                <Button onClick={() => this.remove(k)} style={{float: "right"}}>删除目的地{index + 1}</Button>
+                ) : null}
+            </div>
+        ));
 
         return (
             <DocumentTitle title="创建行程">
@@ -106,6 +174,7 @@ class CreateItinerary extends React.Component<any, any> {
                     <div styleName="routes">
                         <Form onSubmit={this.handleSubmit} style={{width: "520px"}}>
                             <FormItem
+                                style={{padding: "0 10px"}}
                                 label="行程时间"
                                 {...formItemLayout}
                             >
@@ -114,16 +183,22 @@ class CreateItinerary extends React.Component<any, any> {
                                         message: "请输入起止时间!", required: true,
                                     }],
                                 })(
-                                    <RangePicker onChange={this.datePickerOnChange} showTime={true} format="YYYY-MM-DD HH:mm:ss" placeholder={["开始时间", "结束时间"]} />,
+                                    <RangePicker
+                                        onChange={this.datePickerOnChange}
+                                        showTime={true} format="YYYY-MM-DD HH:mm:ss"
+                                        placeholder={["开始时间", "结束时间"]}
+                                    />,
                                 )}
                             </FormItem>
                             <FormItem
+                                style={{padding: "0 10px"}}
                                 label="选择出发地"
                                 {...formItemLayout}
                             >
                                 <LazyOptions getlazyloadCascader={this.getlazyloadCascader} index={1} />
                             </FormItem>
                             <FormItem
+                                style={{padding: "0 10px"}}
                                 label="出发地具体名称"
                                 {...formItemLayout}
                             >
@@ -133,7 +208,6 @@ class CreateItinerary extends React.Component<any, any> {
                                     }],
                                 })(
                                     <AutoComplete
-                                        // dataSource={dataSource1.map((item:any)=>item.fullname)}
                                         style={{ width: "100%" }}
                                         onSelect={(value) => this.onSelect(1, value)}
                                         onSearch={(value) => this.handleSearch(1, value)}
@@ -144,33 +218,12 @@ class CreateItinerary extends React.Component<any, any> {
                                     </AutoComplete>,
                                 )}
                             </FormItem>
-                            <FormItem
-                                label="选择目的地"
-                                {...formItemLayout}
-                            >
-                                <LazyOptions getlazyloadCascader={this.getlazyloadCascader} index={2} />
-                            </FormItem>
-                            <FormItem
-                                label="目的地具体名称"
-                                {...formItemLayout}
-                            >
-                                {getFieldDecorator("endSpot", {
-                                    rules: [{
-                                        message: "输入目的地!", required: true,
-                                    }],
-                                })(
-                                    <AutoComplete
-                                        // dataSource={dataSource2.map((item:any)=>item.fullname)}
-                                        style={{ width: "100%" }}
-                                        onSelect={(value) => this.onSelect(2, value)}
-                                        onSearch={(value) => this.handleSearch(2, value)}
-                                        onChange={(value) => this.spotNameChange(2, value)}
-                                        placeholder="开始地点名称"
-                                    >
-                                        {children2}
-                                    </AutoComplete>,
-                                )}
-                            </FormItem>
+                            {formItems}
+                            <Form.Item {...formItemLayoutWithOutLabel}>
+                                <Button type="dashed" onClick={this.add} style={{ width: "60%" }}>
+                                    <Icon type="plus" /> Add field
+                                </Button>
+                            </Form.Item>
                             <FormItem {...tailFormItemLayout}>
                                 <Button type="primary" htmlType="submit">提交</Button>
                             </FormItem>
