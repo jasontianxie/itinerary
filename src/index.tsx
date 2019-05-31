28d4a9b003e32b482849fb010515eaec51d9af7f
jason <1986tianxie@sina.com>
2019-05-31 12:16:58 +0800

    CI test 1

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import Header from "./components/header/index";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-// add a push to trigger jenkins build
+// add a push to trigger jenkins build test1
 ReactDOM.render(
     <Provider store={store}>
     <HashRouter>
